c4075effcf1ae1805220977bb9e26d0affc28a0a
Luca <lxue02@gmail.com>
2021-02-25 12:14:04 +0000

    left movement

---
+++ ActualProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //make rigidbody field
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //right and left movement
+        if(Input.GetKey(KeyCode.A)){
+            //change rb's velocity to a new 2d vector
+            //x: -5, y: same as it is
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+    }
+}
