1d261db12c4db679c7a618ec53cbbe36c8ebd6a9
Luca <lxue02@gmail.com>
2021-02-25 17:24:41 +0000

    animation method

--- ActualProject/Assets/Scripts/PlayerController.cs
+++ ActualProject/Assets/Scripts/PlayerController.cs
@@ -24,67 +24,75 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
-    }   
-    
+        Animation();
+    }
+
 
     void OnTriggerEnter2D(Collider2D colliderTriggered)
     {
-        if(colliderTriggered.tag == "DeathBox")
+        if (colliderTriggered.tag == "DeathBox")
         {
             SceneManager.LoadScene("MainLevel");
-            print("death box yeyyyy");
         }
     }
 
     void Movement()
     {
- 
         var xAxis = Input.GetAxis("Horizontal");
         var jumpAxis = Input.GetAxis("Jump");
         //right and left movement
         if (xAxis < 0)
         {
-            animator.SetBool("isRunning", true);
             //change rb's velocity to a new 2d vector
             //x: -5, y: same as it is
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-        } 
-
-        if(rb.velocity == new Vector2(0, 0))
-        {
-            animator.SetBool("isRunning", false);
-            animator.SetBool("isJumping", false);
-            animator.SetBool("isFalling", false);
         }
-
 
         if (xAxis > 0)
         {
-            animator.SetBool("isRunning", true);
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-        } 
+        }
 
 
         if (jumpAxis > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 5);
         }
-        
-        if(rb.velocity.y > 0.5)
+    }
+
+    void Animation()
+    {
+
+        //idle
+        if (rb.velocity == new Vector2(0, 0))
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isFalling", false);
+        }
+
+        //if moving to the left or right
+        if (rb.velocity.x > 0.5 || rb.velocity.x < -0.5)
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+
+        //jumping
+        if (rb.velocity.y > 0.5)
         {
             animator.SetBool("isJumping", true);
             animator.SetBool("isFalling", false);
         }
-
-         if(rb.velocity.y < -0.5)
+        //falling
+        if (rb.velocity.y < -0.5)
         {
             animator.SetBool("isJumping", false);
             animator.SetBool("isFalling", true);
 
         }
-
     }
- 
+
 }
