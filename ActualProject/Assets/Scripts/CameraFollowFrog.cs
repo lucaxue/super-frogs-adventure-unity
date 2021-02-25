b514fda60e90e444574de3fb6d5f284b2edba89f
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-02-25 14:34:02 +0000

    cameraFollowFrogFixed

---
+++ ActualProject/Assets/Scripts/CameraFollowFrog.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowFrog : MonoBehaviour
+{
+   public Transform SuperFrog;
+   public float cameraDistance = 30.0f;
+
+   void Awake()
+   { 
+       GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+
+   }
+
+   void FixedUpdate()
+   {
+     transform.position = new Vector3(SuperFrog.position.x, SuperFrog.position.y, transform.position.z);
+   }
+
+
+}
