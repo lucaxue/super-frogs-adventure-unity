32d4aa1f17bf4ddcf8988537bb3995f2b848a040
Luca <lxue02@gmail.com>
2021-02-25 13:59:54 +0000

    box collider ground gaps

--- ActualProject/Assets/Scripts/PlayerController.cs
+++ ActualProject/Assets/Scripts/PlayerController.cs
@@ -16,20 +16,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var xAxis = Input.GetAxis("Horizontal");
+        var jumpAxis = Input.GetAxis("Jump");
         //right and left movement
-        if(Input.GetKey(KeyCode.A)){
+        if (xAxis < 0)
+        {
             //change rb's velocity to a new 2d vector
             //x: -5, y: same as it is
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
         }
 
-        if(Input.GetKey(KeyCode.D)){
+        if (xAxis > 0)
+        {
             rb.velocity = new Vector2(5, rb.velocity.y);
-              transform.localScale = new Vector2(1, 1);
+            transform.localScale = new Vector2(1, 1);
         }
 
-        if(Input.GetKey(KeyCode.Space)){
+        if (jumpAxis > 0)
+        {
             rb.velocity = new Vector2(rb.velocity.x, 5);
         }
     }
