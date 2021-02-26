006a0a5643be5d88f8444d30866c175c0e1216b1
Luca <lxue02@gmail.com>
2021-02-26 12:37:06 +0000

    trying to get StoneFace script movement

--- ActualProject/Assets/Scripts/StoneFaceController.cs
+++ ActualProject/Assets/Scripts/StoneFaceController.cs
@@ -8,22 +8,34 @@ public class StoneFaceController : MonoBehaviour
 
     private Rigidbody2D rbStone;
     void Start()
-    { 
+    {
         rbStone = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement();
-
+        StartCoroutine(Movement());
     }
 
-    IEnumerator Movement() 
+    IEnumerator Movement()
     {
+        //move with velocity
+        //if position is 3 below
+        //stop moving
+        var originalY = rbStone.position.y;
         rbStone.velocity = new Vector2(rbStone.velocity.x, -3);
+        if (rbStone.position == new Vector2(rbStone.position.x, originalY - 1))
+        {
+            rbStone.velocity = new Vector2(0, 0);
+        }
         yield return new WaitForSeconds(2);
         rbStone.velocity = new Vector2(rbStone.velocity.x, 3);
+        if (rbStone.position == new Vector2(rbStone.position.x, originalY + 1))
+        {
+            rbStone.velocity = new Vector2(0, 0);
+        }
+        yield return new WaitForSeconds(2);
     }
 }
